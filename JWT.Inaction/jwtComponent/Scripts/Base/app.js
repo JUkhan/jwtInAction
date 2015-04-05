fde04365005ca665ff36a01dc27bff82abe923ce
JUkhan <jasim.uddin.khan@gmail.com>
2015-04-05 17:55:21 +0600

    charts

--- JWT.Inaction/jwtComponent/Scripts/Base/app.js
+++ JWT.Inaction/jwtComponent/Scripts/Base/app.js
@@ -6,11 +6,11 @@ import {default as controllers} from 'Scripts/app.controllers.js';
 import {default as services} from 'Scripts/app.services.js';
 import {default as directives} from 'Scripts/app.directives.js';
 import {default as filters} from 'Scripts/app.filters.js';
-
+import angularCharts from 'Scripts/Modules/angularCharts/angularCharts.js';
 
 var moduleName='app'; 
 
-angular.module(moduleName,['ui.router', 'ngResource',"ngSanitize",  'LocalStorageModule', 'angular-loading-bar','ui.bootstrap', controllers, services, directives, filters])
+angular.module(moduleName,['ui.router', 'ngResource',"ngSanitize",  'LocalStorageModule', 'angular-loading-bar','ui.bootstrap', controllers, services, directives, filters, angularCharts])
     .factory('authInterceptorService', authInterceptorService)
     .factory('authService', authService)
     .config(config)
@@ -18,7 +18,7 @@ angular.module(moduleName,['ui.router', 'ngResource',"ngSanitize",  'LocalStorag
         $httpProvider.interceptors.push('authInterceptorService');
     })
     .constant('ngAuthSettings', {
-        stsServiceBaseUri: 'http://dacw0066/STS/',        
+        stsServiceBaseUri: 'http://localhost:21545/',        
         apiServiceBaseUri: 'http://dacw0066/WebApi/',
         clientId: 'jwtApp'//nativeApp//jwtApp
     })
