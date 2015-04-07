2467a18dc5d553c30f2890b23ffae1568c081298
jasim <jasim.uddin.khan@gmail.com>
2015-04-07 09:20:25 +0600

    home componenet

--- JWT.Inaction/jwtApp/Scripts/Components/home/homeCtrl.js
+++ JWT.Inaction/jwtApp/Scripts/Components/home/homeCtrl.js
@@ -6,7 +6,11 @@ class homeCtrl extends BaseCtrl
 		super(scope);
 		SVC.set(this, svc);
 		this.title='home';
+      	this.loadData();
 	}
+  	loadData(){
+      SVC.get(this).getData().success(data=>{this.list=data;});
+    }
 }
 homeCtrl.$inject=['$scope', 'homeSvc'];
 export default homeCtrl;
--- JWT.Inaction/jwtApp/Scripts/Components/home/homeSvc.js
+++ JWT.Inaction/jwtApp/Scripts/Components/home/homeSvc.js
@@ -2,13 +2,19 @@ import BaseSvc from 'Scripts/base/BaseSvc.js';
 const HTTP=new WeakMap();
 class homeSvc extends BaseSvc
 {
-	constructor(http){
+	constructor(http, ngAuthSettings){
 		super(http);
 		HTTP.set(this, http);
+      	this.baseUrl=ngAuthSettings.apiServiceBaseUri;
 	}
-	static homeFactory(http)	{
-		return new homeSvc(http);
+  	getData(){
+       
+        return HTTP.get(this).get(this.baseUrl+'api/orders');
+  
+    }
+	static homeFactory(http, ngAuthSettings)	{
+		return new homeSvc(http, ngAuthSettings);
 	}
 }
-homeSvc.homeFactory.$inject=['$http'];
+homeSvc.homeFactory.$inject=['$http','ngAuthSettings'];
 export default homeSvc.homeFactory;
