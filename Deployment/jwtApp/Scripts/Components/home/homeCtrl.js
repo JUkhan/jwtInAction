13b7d07487e080f24d738c336ad13ff7029fedb2
jasim khan <jasimuddinkhan@gmail.com>
2015-04-23 14:53:00 +0600

    init

--- Deployment/jwtApp/Scripts/Components/home/homeCtrl.js
+++ Deployment/jwtApp/Scripts/Components/home/homeCtrl.js
@@ -36,8 +36,8 @@ class homeCtrl extends BaseCtrl
               {"name":"Some data 3","data":[3,1,null,5,2],"connectNulls":true,"id":"series-1"},
               {"name":"Some data 2","data":[5,2,2,3,5],"type":"column","id":"series-2"},
               {"name":"My Super Column","data":[1,1,2,3,2],"type":"column","id":"series-3"}],
-        "title":{"text":"Hello"},
-        "credits":{"enabled":true},
+        "title":{"text":"Sample Charts"},
+        "credits":{"enabled":false},
         "loading":false,"size":{}};
     }
 }
